1489ebadb3e9fb980a616a916983e80ef89346cc
Reshma <Reshma@DESKTOP-M81M8N9>
2022-03-23 17:08:11 +0530

    First commit

---
+++ com.crm.demo/src/main/java/com/crm/demo/CreateContactsTest.java
@@ -0,0 +1,11 @@
+package com.crm.demo;
+
+import org.testng.annotations.Test;
+
+public class CreateContactsTest {
+	@Test
+	public void createCont() {
+		System.out.println("Create Contacts");
+	}
+
+}
