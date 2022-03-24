3d3079d129458be93c160372b7da9afa49a190cc
Reshma <Reshma@DESKTOP-M81M8N9>
2022-03-24 12:32:13 +0530

    User 2 second commit

--- com.crm.demo/src/main/java/com/crm/demo/CreateOrgTest.java
+++ com.crm.demo/src/main/java/com/crm/demo/CreateOrgTest.java
@@ -7,6 +7,14 @@ public class CreateOrgTest {
 	@Test
 	public void createOrg1() {
 		System.out.println("User 1 created organization");
+		
+		System.out.println("User 2 created organization");
+		
+		
+		
+		
+		
+		
 	}
 
 }
