c899ec3b0f05a6e5b51394538874f4e42d848688
Reshma <Reshma@DESKTOP-M81M8N9>
2022-03-24 12:16:11 +0530

    Commit a new class file

---
+++ com.crm.demo/src/main/java/com/crm/demo/CreateOrgTest.java
@@ -0,0 +1,12 @@
+package com.crm.demo;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	
+	@Test
+	public void createOrg1() {
+		System.out.println("User 1 created organization");
+	}
+
+}
