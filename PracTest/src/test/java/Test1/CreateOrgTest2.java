2fc9dcf81e7de9926b314f5f87644425aa23834f
Reshma <Reshma@DESKTOP-M81M8N9>
2022-03-28 17:23:45 +0530

    commit1

---
+++ PracTest/src/test/java/Test1/CreateOrgTest2.java
@@ -0,0 +1,11 @@
+package Test1;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest2 {
+	@Test
+public void Org(){
+	System.out.println("Create Org");
+
+}}
+
---
+++ PracTest/src/test/java/Test1/CreateTest.java
@@ -0,0 +1,10 @@
+package Test1;
+
+import org.testng.annotations.Test;
+
+public class CreateTest {
+	@Test
+	public void test(){
+		System.out.println("hello");
+	}
+}
