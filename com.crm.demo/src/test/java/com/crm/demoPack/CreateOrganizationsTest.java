d00b32e815a16d3af7973fe28aed295f879b1803
Reshma <Reshma@DESKTOP-M81M8N9>
2022-03-28 12:52:45 +0530

    commit

---
+++ com.crm.demo/src/test/java/com/crm/demoPack/CreateOrganizationsTest.java
@@ -0,0 +1,17 @@
+package com.crm.demoPack;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganizationsTest {
+	
+	@Test
+	public void create() {
+		System.out.println("create organizations");
+	}
+	
+	@Test
+	public void delete() {
+		System.out.println("Delete organizations");
+	}
+
+}
